df5995eb924c2403bd70e65f9f7d7f9a36751450
Brayan Rojas <brojas9975@conestogac.on.ca>
2024-02-09 00:42:42 -0500

    DisneyCharacter.h file

--- DisneyCharacter.h
+++ DisneyCharacter.h
@@ -1,1 +1,41 @@
-#pragma once
+#ifndef DISNEYCHARACTER_H
+#define DISNEYCHARACTER_H
+
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+
+using namespace std;
+
+class DisneyCharacter {
+private:
+	string name;
+	string creationDate;
+	int numMovies;
+	char whichPark;
+
+public:
+    // Constructors
+    DisneyCharacter(const string& name, const string& creationDate, int numMovies, char whichPark);
+    DisneyCharacter(const string& name, const string& creationDate);
+
+    // Destructor
+    ~DisneyCharacter();
+
+    // Accessors
+    string getName() const;
+    string getCreationDate() const;
+    int getNumMovies() const;
+    char getWhichPark() const;
+
+    // Mutators
+    bool setNumMovies(int numMovies);
+    bool setWhichPark(char whichPark);
+
+    // Public methods
+    void ShowInfo() const;
+    bool PlaceCharacter(char whichPark);
+    void SameMovies(DisneyCharacter& anotherCharacter);
+};
+
+#endif // DISNEYCHARACTER_H
