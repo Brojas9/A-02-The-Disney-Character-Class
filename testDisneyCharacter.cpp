ae549535ad6f2224c09a9e8fb9999fc6ebac6ead
Brayan Rojas <brojas9975@conestogac.on.ca>
2024-02-10 11:14:00 -0500

    Updating testDisneyCharacter.cpp file

--- testDisneyCharacter.cpp
+++ testDisneyCharacter.cpp
@@ -1,8 +1,16 @@
+/* -- this is a FILEHEADER COMMENT --
+    NAME	:	TestDisneyCharacter
+    PURPOSE :	This comment space is meant to indicate the purpose of
+                this application.  In the description, you can indicate which
+                supporting classes you are using - but the bulk of this
+                comment is meant to indicate the purpose of this program
+                (i.e. what the user is able to do with this program, etc.)
+
+*/
 #include "DisneyCharacter.h"
-#include <iostream>
 
-int main() {
-
+int main() 
+{
     DisneyCharacter mickey("Mickey", "1929-01-01", 100, 'M');
     DisneyCharacter minnie("Minnie", "1930-01-01");
 
@@ -10,14 +18,15 @@ int main() {
     minnie.SameMovies(mickey);
 
     // Dump member values for Mickey and Minnie
-    cout << "Mickey's Info:" << endl;
+    printf("Mickey's Info: \n");
     mickey.ShowInfo();
 
-    cout << "\nMinnie's Info:" << endl;
+    printf("Minnie's Info: \n");
     minnie.ShowInfo();
 
     // Place Minnie in Epcot park
     minnie.PlaceCharacter('E');
+    minnie.ShowInfo();
 
     return 0;
 }
