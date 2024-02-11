a3c230ba78d9e12ee873e7e9a040552389fb095d
Brayan Rojas <brojas9975@conestogac.on.ca>
2024-02-10 19:18:17 -0500

    Commenting DisneyCharacter.cpp file

--- DisneyCharacter.cpp
+++ DisneyCharacter.cpp
@@ -1,65 +1,121 @@
 #include "DisneyCharacter.h"
 /*  -- Method Header Comment
     Name	: DisneyCharacter -- CONSTRUCTOR
-    Purpose : to instantiate a new DisneyCharacter object - given a set of attribute values
-    Inputs	:	newName			String		name of the DisneyCharacter
-                newBreed		String		breed of the dog
-                newHairLength	int			hairless, short, medium, long
-                newSize			int			Toy, Small, Medium, Large
+    Purpose : The purpose of this constructor is to instantiate (initialize) all 
+              the member variables of the DisneyCharacter class. 
+    Inputs	:	Name			    String		Name of the DisneyCharacter
+                CreationDate		String		CreationDate of the DisneyCharacter
+                NumMovies	        int			Number of movies
+                whichPark			char		Initial of the park's name.
     Outputs	:	NONE
     Returns	:	Nothing
 */
-
-// Constructor with all data members
 DisneyCharacter::DisneyCharacter(const char* name, const char* creationDate, int numMovies, char whichPark): numMovies(numMovies), whichPark(whichPark)
 {   
     // Truncate name if longer than 50 characters
-    strncpy(this->name, name, 50);
-    this->name[50] = '\0';
+    strncpy(this->name, name, maxLenN);
+    this->name[maxLenN - nullT] = '\0';
+    //If name is longer than 46 characters, append " …"
+    if (strlen(name) > 46) 
+    {
+        strcat(this->name, " …"); 
+    }
+
+    // CreationDate not longer than 10 characters
+    strncpy(this->creationDate, creationDate, maxLenD);
+    this->creationDate[maxLenD - nullT] = '\0';
    
-    strncpy(this->creationDate, creationDate, 10);
-    this->creationDate[10] = '\0';
-   
-    // Ensure whichPark is valid, mark character as not placed if invalid
-    if (whichPark != 'M' && whichPark != 'S' && whichPark != 'A' && whichPark != 'E' && whichPark 
-        != 'C' && whichPark != 'N') 
+    // Checking if the initial of whichpark is valid.
+    if (whichPark != 'M' && whichPark != 'S' && whichPark != 'A' && whichPark != 'E' && whichPark != 'C' && whichPark != 'N') 
     {
         this->whichPark = 'N';
     }
 }
 
-// Constructor with default values for numMovies and whichPark
-DisneyCharacter::DisneyCharacter(const char* name, const char* creationDate)
-    : DisneyCharacter(name, creationDate, 0, 'N') {}
 
-// Destructor
+/*  -- Method Header Comment
+    Name	: DisneyCharacter -- CONSTRUCTOR
+    Purpose : Constructor for numMovies and whichPark
+    Inputs	:	NumMovies	        int			 0
+                whichPark			char	    'N'
+    Outputs	:	NONE
+    Returns	:	Nothing
+*/
+DisneyCharacter::DisneyCharacter(const char* name, const char* creationDate): DisneyCharacter(name, creationDate, 0, 'N') {}
+
+
+/*  -- Method Header Comment
+    Name	: DisneyCharacter -- DESTRUCTOR
+    Purpose : The purpose is to destroy the DisneyCharacter object - free up the memory associated with the object
+    Inputs	:	NONE
+    Outputs	:	outputs a final message from the object before being destroyed
+    Returns	:	Nothing
+*/
 DisneyCharacter::~DisneyCharacter()
 {
     printf("%s destroyed.\n", name);
 }
 
-// Accessors
+
+/*  -- Method Header Comment
+    Name	:	getName
+    Purpose :	is to provide access to the private member variable name, which stores the name of the Disney character.
+    Inputs	:	NONE
+    Outputs	:	NONE
+    Returns	:	char* name
+*/
 char* DisneyCharacter::getName(void) 
 {
     return name;
 }
 
+
+/*  -- Method Header Comment
+    Name	:	getCreationDate
+    Purpose :	is to provide access to the private member variable CreationDate, which stores the CreationDate of the Disney character.
+    Inputs	:	NONE
+    Outputs	:	NONE
+    Returns	:	char* CreationDate
+*/
 char* DisneyCharacter::getCreationDate(void) 
 {
     return creationDate;
 }
 
-int DisneyCharacter::getNumMovies() 
+
+/*  -- Method Header Comment
+    Name	:	getNumMovies
+    Purpose :	is to provide access to the private member variable numMovies, which stores the numMovies of the Disney character.
+    Inputs	:	NONE
+    Outputs	:	NONE
+    Returns	:	int Number of Movies
+*/
+int DisneyCharacter::getNumMovies(void) 
 {
     return numMovies;
 }
 
-char DisneyCharacter::getWhichPark() 
+
+/*  -- Method Header Comment
+    Name	:	getWhichPark
+    Purpose :	is to provide access to the private member variable whichPark, which stores the whichPark initial of the Disney character.
+    Inputs	:	NONE
+    Outputs	:	NONE
+    Returns	:	char initial of Disney character park.
+*/
+char DisneyCharacter::getWhichPark(void) 
 {
     return whichPark;
 }
 
-// Mutators
+
+/*  -- Method Header Comment
+    Name	:	setNumMovies
+    Purpose :	To set the number of movies for the Disney character.****************
+    Inputs	:	int numMovies
+    Outputs	:	NONE
+    Returns	:	bool 
+*/
 bool DisneyCharacter::setNumMovies(int numMovies) 
 {
     if (numMovies >= 0) 
@@ -70,10 +126,17 @@ bool DisneyCharacter::setNumMovies(int numMovies)
     return false;
 }
 
+
+/*  -- Method Header Comment
+    Name	:	setWhichPark
+    Purpose :	To set the park designation for the Disney character.
+    Inputs	:	char whichPark
+    Outputs	:	NONE
+    Returns	:	bool
+*/
 bool DisneyCharacter::setWhichPark(char whichPark) 
 {
-    if (whichPark == 'M' || whichPark == 'S' || whichPark == 'A' 
-        || whichPark == 'E' || whichPark == 'C' || whichPark == 'N') 
+    if (whichPark == 'M' || whichPark == 'S' || whichPark == 'A' || whichPark == 'E' || whichPark == 'C' || whichPark == 'N') 
     {
         this->whichPark = whichPark;
         return true;
@@ -81,7 +144,14 @@ bool DisneyCharacter::setWhichPark(char whichPark)
     return false;
 }
 
-// Public methods
+
+/*---Public methods /memeber functions
+    Name	: ShowInfo
+    Purpose :   To display information about the Disney character.
+    Inputs	:	NONE
+    Outputs	:	Displays the name, creation date, number of movies, and park designation of the Disney character.
+    Returns	:	Nothing
+*/
 void DisneyCharacter::ShowInfo() 
 {
     printf("Name: %s\n", name);
@@ -90,6 +160,14 @@ void DisneyCharacter::ShowInfo()
     printf("Which Park: %c\n\n", whichPark);
 }
 
+
+/*  -- Mutator Header Comment
+    Name	:	PlaceCharacter
+    Purpose :	To assign a park designation for the Disney character.
+    Inputs	:	char whichPark
+    Outputs	:	NONE
+    Returns	:	bool
+*/
 bool DisneyCharacter::PlaceCharacter(char whichPark) 
 {
     if (setWhichPark(whichPark)) 
@@ -99,6 +177,14 @@ bool DisneyCharacter::PlaceCharacter(char whichPark)
     return false;
 }
 
+
+/*  -- Mutator Header Comment
+    Name	:	SameMovies
+    Purpose :	To set the number of movies for this Disney character equal to another character's number of movies.
+    Inputs	:	DisneyCharacter& anotherCharacter
+    Outputs	:	NONE
+    Returns	:	bool
+*/
 void DisneyCharacter::SameMovies(DisneyCharacter& anotherCharacter) 
 {
     numMovies = anotherCharacter.getNumMovies();
