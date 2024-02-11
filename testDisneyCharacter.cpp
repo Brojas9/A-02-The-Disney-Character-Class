0884f6b8b5859cce9623c44ec2b295eb36cb8062
Brayan Rojas <brojas9975@conestogac.on.ca>
2024-02-10 19:19:37 -0500

    Commenting testDisneyCharacter.cpp's file

--- testDisneyCharacter.cpp
+++ testDisneyCharacter.cpp
@@ -1,30 +1,27 @@
 /* -- this is a FILEHEADER COMMENT --
-    NAME	:	TestDisneyCharacter
-    PURPOSE :	This comment space is meant to indicate the purpose of
-                this application.  In the description, you can indicate which
-                supporting classes you are using - but the bulk of this
-                comment is meant to indicate the purpose of this program
-                (i.e. what the user is able to do with this program, etc.)
-
+    NAME	:	DisneyCharacter
+    PURPOSE :	The purpose of this application is to demonstrate 
+                the use of object-oriented programming concepts in C++. 
+                This program implement a class named DisneyCharacter which
+                represent characters from the Disney universe.
 */
 #include "DisneyCharacter.h"
-
+#include <stdio.h>
 int main() 
 {
     DisneyCharacter mickey("Mickey", "1929-01-01", 100, 'M');
     DisneyCharacter minnie("Minnie", "1930-01-01");
 
-    // Set number of Minnie's movies equal to Mickey's
+    // Number of Minnie's movies equal to Mickey's
     minnie.SameMovies(mickey);
 
-    // Dump member values for Mickey and Minnie
     printf("Mickey's Info: \n");
     mickey.ShowInfo();
 
     printf("Minnie's Info: \n");
     minnie.ShowInfo();
 
-    // Place Minnie in Epcot park
+    // Placing Minnie in Epcot park
     minnie.PlaceCharacter('E');
     minnie.ShowInfo();
 
